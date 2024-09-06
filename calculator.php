da5bbd0b8f9628d641e87c9b0119e986bf4a0925
Daniel Schnelle <daniel_schnelle@web.de>
2024-09-06 13:59:12 +0200

    Simple subtraction

--- calculator.php
+++ calculator.php
@@ -2,9 +2,14 @@
 
 function calculate(string $term) {
     $sum = 0;
+    $term = str_replace("-","+-", $term);
     $summands = explode("+", $term);
     foreach($summands as $summand) {
-        $sum += $summand;
+        if ($summand[0] == "-") {
+            $sum -= substr($summand, 1);
+        } else {
+            $sum += $summand;
+        }
     }
     return $sum;
 }
--- tests.php
+++ tests.php
@@ -5,7 +5,7 @@ require_once 'calculator.php';
 function runTests() {
     $success = true;
     $success &= assert(calculate("1+1") == 2, "Test '1+1' failed");
-
+    $success &= assert(calculate("7-3") == 4, "Test '7-3' failed");
     if ($success) echo "All tests succeeded";
 }
 

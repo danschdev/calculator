77f82f4d882c2617d14acfd557aa22cd02d73b23
Daniel Schnelle <daniel_schnelle@web.de>
2024-09-06 14:05:29 +0200

    Add tests for chained sum, integers without operator

--- tests.php
+++ tests.php
@@ -6,6 +6,10 @@ function runTests() {
     $success = true;
     $success &= assert(calculate("1+1") == 2, "Test '1+1' failed");
     $success &= assert(calculate("7-3") == 4, "Test '7-3' failed");
+    $success &= assert(calculate("1+4-3") == 2, "Test '1+4-3' failed");
+    $success &= assert(calculate("5") == 5, "Test '5' failed");
+    $success &= assert(calculate("0-17") == -17, "Test '0-17' failed");
+    $success &= assert(calculate("-13") == -13, "Test '-13' failed");
     if ($success) echo "All tests succeeded";
 }
 
